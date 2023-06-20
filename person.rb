f820210183255db4459ecfcd63b63a30b47cd97a
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-06-20 10:02:01 +0300

    Move attr_accessors to the top of class constructor

--- person.rb
+++ person.rb
@@ -1,13 +1,13 @@
 class Person
+  attr_accessor :name, :age
+  attr_reader :id, :parent_permission
+
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age.to_i
     @parent_permission = parent_permission
   end
-
-  attr_accessor :name, :age
-  attr_reader :id, :parent_permission
 
   private
 
