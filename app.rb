3b04f0725754819b2252d055d29cbdd6ec9c47d7
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-06-23 15:34:43 +0300

    Refactor begin method to reduce size

--- app.rb
+++ app.rb
@@ -13,26 +13,40 @@ class App
   end
 
   def begin
-    puts 'Welcome to School Library App!'
-    puts "\n"
-
-    while true
-      puts 'Please choose an option by entring a number:'
-      puts '1 - List all books'
-      puts '2 - List all people'
-      puts '3 - Create a person'
-      puts '4 - Create a book'
-      puts '5 - Create a rental'
-      puts '6 - List all rentals for a given person id'
-      puts '7 - Exit'
+    puts "Welcome to School Library App!\n"
+    command = 0
+    while command != 7
+      show_menu
       command = gets.chomp
-      list_books if command == '1'
-      list_people if command == '2'
-      create_person if command == '3'
-      create_book if command == '4'
-      create_rental if command == '5'
-      list_rentals_by_id if command == '6'
-      break if command == '7'
+      operate(command)
+    end
+  end
+
+  def show_menu
+    puts 'Please choose an option by entring a number:'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+  end
+
+  def operate(command)
+    case command
+    when '1'
+      list_books
+    when '2'
+      list_people
+    when '3'
+      create_person
+    when '4'
+      create_book
+    when '5'
+      create_rental
+    when '6'
+      list_rentals_by_id
     end
   end
 
