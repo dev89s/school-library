fb4f1d428aec4235bf9398f69ff99060b05c16bc
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-06-27 15:35:10 +0300

    Refactor code & add preserve data methods

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require './student'
 require './teacher'
 require './book'
 require './rental'
+require 'json'
 
 class App
   attr_accessor :books, :persons, :rentals
@@ -110,5 +111,38 @@ class App
     @rentals.each do |rental|
       puts "Date: #{rental.date}, Book: #{rental.book.title} by #{rental.book.author}" if rental.person.id == id
     end
+  end
+
+  def preserve_data
+    preserve_books if @books.length.positive?
+    preserve_persons if @persons.length.positive?
+    preserve_rentals if @rentals.length.positive?
+  end
+
+  def preserve_books
+    books = @books.map { |book| [book.title, book.author] }
+    file = File.open('books.json', 'w')
+    file.write(books.to_json)
+  end
+
+  def preserve_persons
+    persons = @persons.map do |person|
+      if person.instance_of?(Teacher)
+        ['Teacher', person.name, person.age, person.specialization]
+      elsif person.instance_of?(Student)
+        ['Student', person.name, person.age, person.parent_permission]
+      end
+    end
+    file = File.open('persons.json', 'w')
+    file.write(persons.to_json)
+  end
+
+  def preserve_rentals
+    rentals = @rentals.map do |rental|
+      [rental.date, rental.person.name, rental.book.title]
+    end
+    puts rentals
+    file = File.open('rentals.json', 'w')
+    file.write(rentals.to_json)
   end
 end
